7f66172da5e3aba02ce3f5abc6f43f132d7696d7
jadenfigger <jadenfigger@gmail.com>
2021-05-12 12:48:37 -0700

    Add files via upload

---
+++ Pages/GamesPage.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using GameData;
+
+namespace GameWebsite.Pages
+{
+    public class GamesPageModel : PageModel
+    {
+		public readonly GameDataHolder games;
+		private readonly ILogger<GamesPageModel> _logger;
+
+        public GamesPageModel(ILogger<GamesPageModel> logger)
+        {
+            _logger = logger;
+			games = new GameDataHolder();
+		}
+
+        public void OnGet()
+        {
+        }
+    }
+}
